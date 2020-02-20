4a31655660ec6322f9ef168e9ed6f3a39491fb95
bradley-r-martin <bradley.r.martin@me.com>
2020-02-20 12:06:44 +1100

    Init

--- FrameworkServiceProvider.php
+++ FrameworkServiceProvider.php
@@ -9,7 +9,7 @@ class FrameworkServiceProvider extends ServiceProvider
 
 
     public function register(){
-        $this->app->register(\Dusterio\AwsWorker\Integrations\LaravelServiceProvider::class);
+        $this->app->register(\FoxxMD\LaravelElasticBeanstalkQueueWorker\ElasticBeanstalkQueueWorkerProvider::class);
     }
     public function boot(\Illuminate\Routing\Router $router, \Illuminate\Contracts\Http\Kernel $kernel){
         $kernel->prependMiddleware(\BRM\ElasticBeanstalk\app\Middleware\ElasticBeanstalk::class);
