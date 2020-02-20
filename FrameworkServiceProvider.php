29c50e4bcd971dd1a932ca57c43d66a321119b04
bradley-r-martin <bradley.r.martin@me.com>
2020-02-20 11:33:43 +1100

    Init

--- FrameworkServiceProvider.php
+++ FrameworkServiceProvider.php
@@ -7,8 +7,15 @@ use Illuminate\Support\ServiceProvider;
 class FrameworkServiceProvider extends ServiceProvider
 {
 
-    public function boot(\Illuminate\Routing\Router $router, \Illuminate\Contracts\Http\Kernel $kernel)
-    {
+
+    public function register(){
+        $this->app->register(\Dusterio\AwsWorker\Integrations\LaravelServiceProvider::class);
+    }
+    public function boot(\Illuminate\Routing\Router $router, \Illuminate\Contracts\Http\Kernel $kernel){
         $kernel->prependMiddleware(\BRM\ElasticBeanstalk\app\Middleware\ElasticBeanstalk::class);
     }
+
+
+    
+
 }
