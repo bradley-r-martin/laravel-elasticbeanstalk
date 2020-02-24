79f9c8a7b2957abe5bc09d1376cd6ad027621ff1
bradley-r-martin <bradley.r.martin@me.com>
2020-02-24 22:13:24 +1100

    Init

--- FrameworkServiceProvider.php
+++ FrameworkServiceProvider.php
@@ -10,10 +10,17 @@ class FrameworkServiceProvider extends ServiceProvider
 
     public function register(){
         $this->app->register(\FoxxMD\LaravelElasticBeanstalkQueueWorker\ElasticBeanstalkQueueWorkerProvider::class);
+   
+        config([
+          'cache.default' => 'database'
+        ]);
+   
     }
     public function boot(\Illuminate\Routing\Router $router, \Illuminate\Contracts\Http\Kernel $kernel){
         $kernel->prependMiddleware(\BRM\ElasticBeanstalk\app\Middleware\ElasticBeanstalk::class);
 
+        $this->loadMigrationsFrom(__DIR__.'/app/Database/Migrations');
+      
         $this->publishes([
           __DIR__.'/app/ebextensions/' => base_path('.ebextensions'),
         ], 'laravel-eb');
