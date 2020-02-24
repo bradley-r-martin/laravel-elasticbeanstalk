cf1fbae4471c29b4f1a548566655b3b0544144f3
bradley-r-martin <bradley.r.martin@me.com>
2020-02-24 15:59:23 +1100

    Init

--- FrameworkServiceProvider.php
+++ FrameworkServiceProvider.php
@@ -13,9 +13,10 @@ class FrameworkServiceProvider extends ServiceProvider
     }
     public function boot(\Illuminate\Routing\Router $router, \Illuminate\Contracts\Http\Kernel $kernel){
         $kernel->prependMiddleware(\BRM\ElasticBeanstalk\app\Middleware\ElasticBeanstalk::class);
+
+        $this->publishes([
+          __DIR__.'/app/ebextensions/' => base_path('.ebextensions'),
+        ], 'laravel-eb');
     }
-
-
-    
 
 }
